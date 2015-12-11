0112b01e074a63f24afdc657d1899cfc0d9fc970
Yaroslav <kidjimoshi96@gmail.com>
2015-12-11 05:27:37 +0200

    full project with db azure

--- config/db_params.php
+++ config/db_params.php
@@ -2,15 +2,15 @@
 
 return array(
 
-//    'host' => 'us-cdbr-azure-west-c.cloudapp.net',
-//    'dbname' => 'Time2ReadDB',
-//    'user' => 'bab828921052bc',
-//    'password' => 'da0efa3c'
-
-    'host' => 'localhost',
+    'host' => 'us-cdbr-azure-west-c.cloudapp.net',
     'dbname' => 'Time2ReadDB',
-    'user' => 'root',
-    'password' => 'bcdwloqt'
+    'user' => 'bab828921052bc',
+    'password' => 'da0efa3c'
+
+ //   'host' => 'localhost',
+//    'dbname' => 'Time2ReadDB',
+//    'user' => 'root',
+ //   'password' => 'bcdwloqt'
 
 );
 
